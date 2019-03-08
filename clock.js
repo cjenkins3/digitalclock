bfc20af0e6c2a25d844f7fe49c427c60b5527d37
Jenkins <cajenkins@smu.edu>
2019-03-08 15:50:02 -0600

    deploy

---
+++ clock.js
@@ -0,0 +1,25 @@
+//after coming up with const, later put all in function
+//to enable real-time clock motions
+function clock() {
+    const fullDate = new Date();
+    var hours = fullDate.getHours();
+    var mins = fullDate.getMinutes();
+    var secs = fullDate.getSeconds();
+
+if (hours < 10) {
+    hours = "0" + hours;
+}
+if (mins < 10) {
+    mins = "0" + mins;
+}
+if (secs < 10) {
+    secs = "0" + secs;
+}
+
+//
+    document.getElementById("hour").innerHTML = hours;
+    document.getElementById("minute").innerHTML = ":" + mins;
+    document.getElementById("second").innerHTML = ":" + secs;
+}
+//sets constant timing for the clock
+setInterval(clock, 100);
